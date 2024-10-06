4f7785b6d13c1458e8659fa8b2197a8041ebf6e8
Levchenko Aleksandr <madlion@mail.ru>
2024-10-06 17:09:29 +0700

    review #1 Changes to be committed: 	modified:   app/core/config.py 	renamed:    app/services/constants.py -> app/core/constants.py 	modified:   app/services/google_api.py

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -2,61 +2,63 @@ from datetime import datetime, timedelta
 
 from aiogoogle import Aiogoogle
 
-from app.services.constants import (
-    SECONDS_IN_HOUR,
-    SECONDS_IN_MINUTE,
-    ROW_COUNT, COLUMN_COUNT
-)
+from app.core.constants import SECONDS_IN_HOUR, SECONDS_IN_MINUTE
 from app.core.config import settings
 from app.models.charity_project import CharityProject
 
 
-FORMAT = "%Y/%m/%d %H:%M:%S"
-
-
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-    now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_services.discover('sheets', 'v4')
+    now_date_time = datetime.now().strftime(settings.format_datetime)
+    service = await wrapper_services.discover(
+        'sheets',
+        settings.google_sheets_api_version
+    )
     spreadsheet_body = {
         'properties': {'title': f'Отчёт от {now_date_time}',
-                       'locale': 'ru_RU'},
+                       'locale': settings.sheets_locale},
         'sheets': [
-            {'properties': {'sheetType': 'GRID',
-                            'sheetId': 0,
-                            'title': 'Лист1',
-                            'gridProperties': {'rowCount': ROW_COUNT,
-                                               'columnCount': COLUMN_COUNT}}}]
+            {'properties': {
+                'sheetType': settings.sheet_type,
+                'sheetId': settings.sheet_id,
+                'title': settings.sheet_title,
+                'gridProperties': {'rowCount': settings.row_count,
+                                   'columnCount': settings.column_count}}}]
     }
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    spreadsheetid = response['spreadsheetId']
-    return spreadsheetid
+    spreadsheet_id = response['spreadsheetId']
+    return spreadsheet_id
 
 
 async def set_user_permissions(
-        spreadsheetid: str,
+        spreadsheet_id: str,
         wrapper_services: Aiogoogle
 ) -> None:
-    permissions_body = {'type': 'user',
-                        'role': 'writer',
+    permissions_body = {'type': settings.permissions_type,
+                        'role': settings.permissions_role,
                         'emailAddress': settings.email}
-    service = await wrapper_services.discover('drive', 'v3')
+    service = await wrapper_services.discover(
+        'drive', settings.google_drive_api_version
+    )
     await wrapper_services.as_service_account(
         service.permissions.create(
-            fileId=spreadsheetid,
+            fileId=spreadsheet_id,
             json=permissions_body,
-            fields="id"
+            fields='id'
         ))
 
 
 async def spreadsheets_update_value(
-        spreadsheetid: str,
+        spreadsheet_id: str,
         projects: list[CharityProject],
         wrapper_services: Aiogoogle
 ) -> None:
-    now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_services.discover('sheets', 'v4')
+    now_date_time = datetime.now().strftime(settings.format_datetime)
+    service = await wrapper_services.discover(
+        'sheets',
+        settings.google_sheets_api_version
+    )
     table_values = [
         ['Отчёт от', now_date_time],
         ['Топ проектов по скорости закрытия'],
@@ -65,27 +67,27 @@ async def spreadsheets_update_value(
     for project in projects:
         new_row = [
             str(project.name),
-            str(get_timedelta(project)),
+            str(get_time_difference(project)),
             str(project.description)
         ]
         table_values.append(new_row)
 
     update_body = {
-        'majorDimension': 'ROWS',
+        'majorDimension': settings.update_major_dimension,
         'values': table_values
     }
     response = await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
-            range='A1:E30',
-            valueInputOption='USER_ENTERED',
+            spreadsheetId=spreadsheet_id,
+            range=settings.sheets_range,
+            valueInputOption=settings.sheets_value_input_option,
             json=update_body
         )
     )
     return response
 
 
-def get_timedelta(project: CharityProject) -> str:
+def get_time_difference(project: CharityProject) -> str:
     time_difference: timedelta = project.close_date - project.create_date
     days = time_difference.days
     hours, remainder = divmod(time_difference.seconds, SECONDS_IN_HOUR)
